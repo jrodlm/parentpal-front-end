67836966d8e666abdf23b16229457ea8d15997f9
mbatamuliza <sheisladyblac@gmail.com>
2025-05-20 18:59:00 -0500

    final changes on signup and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import react, { useState } from 'react';
+
 
 export default function Login() {
-    console.log('Login page rendered');
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
 
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
+    const handleChange = (e) => {
+        e.preventDefault({...formData, [e.EventTarget.name]: e.target.value});
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:3000/api/auth/login',{
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await res.json();
+            console.log('Login response:', err);
+        } catch (err) {
+
+        }
+    };
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Log In</h2>
+            <input name="email" placeholder="Email" type="email" onCanPlay={handleChange} required />
+            <input name="password" placeholder="password" type="password" onChange={handleChange} required/>
+            <button type="submit">Log In</button>
+        </form>
+    );
 }

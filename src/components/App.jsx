6ae926754ff36edb686630fcd35676fdc36ee067
mbatamuliza <sheisladyblac@gmail.com>
2025-05-20 19:42:58 -0500

    made chages signup and login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import NavBar from './NavBar';
+import ActivityLog from '../../../parentpal-back-end/models/ActivityLog';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/test" element={<div>Test route working!</div>} />
+        <Route path="/activities" element={<ActivityLog />} />
      </Routes>
   </>
   );

c0d7cbaa610c4a9d92d2c5f5cf7687b5dedc2561
jrodlm <jarodlmorris@gmail.com>
2025-05-21 20:13:37 -0400

    added placeholder components folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/test" element={<div>Test route working!</div>} />
      </Routes>
   </>
   );
